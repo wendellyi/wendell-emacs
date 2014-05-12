1a64d1a09d6384f477192cd14ee9062f24caac64
wendellyi <wendellyi@yeah.net>
2014-05-12 13:50:43 +0800

    1.修改注释。

--- emacs.el
+++ emacs.el
@@ -4,9 +4,9 @@
 (add-to-list 'load-path "~/.emacs.d")
 
 ;; 加载各个子功能
-(load "init-base.el")				            ; 基本设置
-;; (load "init-theme.el")                          ; 主题设置，命令行可以注释掉
-(load "init-kbd.el")                            ; 全局键位绑定
-(load "init-language.el")                       ; 编程语言相关设定
-(load "init-auto-complete.el")                  ; 自动补全
+(load "init-base.el")                   ; 基本设置
+;; (load "init-theme.el")                  ; 主题设置，命令行可以注释掉
+(load "init-kbd.el")                    ; 全局键位绑定
+(load "init-language.el")               ; 编程语言相关设定
+(load "init-auto-complete.el")          ; 自动补全
 
